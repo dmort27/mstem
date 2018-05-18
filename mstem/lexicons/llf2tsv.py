2a45ee8443ae30f63075ff622968c82831d6f5c0
David R. Mortensen <davidmortensen@gmail.com>
2018-05-18 11:52:52 -0400

    Added epitran support

--- mstem/lexicons/llf2tsv.py
+++ mstem/lexicons/llf2tsv.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from lxml import etree
+import epitran
 import csv
 import sys
 
 
-def main(fnin, fnout):
+def main(mode, fnin, fnout):
+    epi = epitran.Epitran(mode)
     tree = etree.parse(fnin)
     root = tree.getroot()
     with open(fnout, 'w', encoding='utf-8') as fout:
@@ -13,8 +15,8 @@ def main(fnin, fnout):
         for entry in root.xpath('//ENTRY'):
             lemma = entry.find('LEMMA').text
             gloss = entry.find('GLOSS').text
-            writer.writerow([lemma, gloss])
+            writer.writerow([epi.transliterate(lemma), gloss])
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
